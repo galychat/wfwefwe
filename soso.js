5861982bcc02d6de6b0393bc6f699d0ecfd7fdbf
galychat <42301824+galychat@users.noreply.github.com>
2018-08-30 19:00:33 +0300

    Create soso.js

---
+++ soso.js
@@ -0,0 +1,27 @@
+}
+
+.ae.label.label-primary.fa.fa-gear{border: 1px solid #FFF947;border-radius:7px 7px 15px 15px}
+.ae.fa.chats.label.fa-comment.label-warning{border-bottom: 1px solid #FFF947;
+    border-top: 1px solid #FFF947;
+    border-Right: 1px solid #FFF947;border-left: 1px solid #FFF947;margin-left:-1px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.label.fa-commenting-o.label-primary{border-bottom: 1px solid #FFF947;
+    border-top: 1px solid #FFF947;
+    border-Right: 1px solid #FFF947;border-left: 1px solid #FFF947;margin-left:-2px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.label.fa-commenting-o.label-warning{border-bottom: 1px solid #FFF947;
+    border-top: 1px solid #FFF947;
+    border-Right: 1px solid #FFF947;border-left: 1px solid #FFF947;margin-left:-2px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.chats.label.fa-comment.label-primary{border-bottom: 1px solid #FFF947;
+    border-top: 1px solid #FFF947;
+    border-Right: 1px solid #FFFFFF;border-left: 1px solid #FFF947;margin-left:-2px!important;border-radius:0px 7px 15px 0px}
+.ae.fa.label.label-primary.fa-users{border-bottom: 1px solid #FFF947;
+    border-top: 1px solid #FFF947;
+    border-Right: 1px solid #FFF947;border-left: 1px solid #FFF947;border-radius:7px 0px 0px 15px}
+.ae.fa.label.label-primary.fa-user{border-bottom: 1px solid #FFF947;
+    border-top: 1px solid #FFF947;
+    border-Right: 1px solid #FFF947;border-left: 1px solid #FFF947;border-radius:7px 0px 0px 15px}
+
+</style>
+`).insertBefore('body'),
+clearInterval(myVar);
+}else{console.log(usmsgw)} }, 2000);
+})
